a15ceeeaf6f252adf3b9322f94f1028f482d8525
jadon6uwizeye <jeandedieuuwizeye6@gmail.com>
2023-10-22 20:16:21 +0200

    Remove unnecessary code comments

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,3 +1,4 @@
 export const PUBLIC = 'PUBLIC';
 export const PRIVATE = 'PRIVATE';
 export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
+export const ADMIN_LOGIN = 'ADMIN-LOGIN'
--- src/routes/public/index.jsx
+++ src/routes/public/index.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { CHANGE_PASSWORD } from '../../assets/constants'
-import Login from '../../dashboards/supportFeatures/authentication/components/Login'
-import ChangePassword from '../../dashboards/supportFeatures/authentication/components/ChangePassword'
-import AdminLogin from '../../dashboards/adminFeatures/authentication/components/AdminLogin'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { CHANGE_PASSWORD, ADMIN_LOGIN } from "../../assets/constants";
+import Login from "../../dashboards/supportFeatures/authentication/components/Login";
+import ChangePassword from "../../dashboards/supportFeatures/authentication/components/ChangePassword";
+import AdminLogin from "../../dashboards/adminFeatures/authentication/components/AdminLogin";
 
 function Public() {
   return (
     <>
-        <Routes>
-            <Route index element={<Login />} />
-            <Route index element={<AdminLogin />} />
-            <Route path={CHANGE_PASSWORD} element={<ChangePassword />} />
-        </Routes>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path={ADMIN_LOGIN} element={<AdminLogin />} />
+        <Route path={CHANGE_PASSWORD} element={<ChangePassword />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default Public
+export default Public;
