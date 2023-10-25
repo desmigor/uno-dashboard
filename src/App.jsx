2b26e8c7b0609c5d48faf51981569c3585ecf125
Richard NSHIMYUMUKIZA <48586076+ricahcyuzuzo@users.noreply.github.com>
2023-10-25 16:13:20 +0200

    Revert "Ft dashboard support"

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,27 @@ import SupportLogin from './dashboards/supportFeatures/authentication/components
 import Login from './dashboards/adminFeatures/authentication/components/AdminLogin';
 import ResetPassword from './dashboards/supportFeatures/authentication/components/ResetPassword';
 import ChangePassword from './dashboards/supportFeatures/authentication/components/ChangePassword';
-import Pending from './dashboards/supportFeatures/pending/components';
-import Packages from './dashboards/supportFeatures/packages/components';
-import Couriers from './dashboards/supportFeatures/couriers/components';
-import Settings from './dashboards/supportFeatures/settings/components';
-import Layout from './routes/private/layout';
 import Dashboard from './dashboards/supportFeatures/dashboard/components';
+import Layout from './routes/private/layout';
 
 function App() {
 
-
   return (
+    <div>
       <Router>
         <Routes>
-          <Route>
-            <Route path='/' element={<SupportLogin />} />
-            <Route path='/reset' element={<ResetPassword />} />
-            <Route path='/change-password' element={<ChangePassword />} />
-            <Route path='/admin' element={<Login />} />
-          </Route>
+          <Route path='/' element={<SupportLogin />} />
+
+          <Route path='/reset' element={<ResetPassword />} />
+          <Route path='/change-password' element={<ChangePassword />} />
+          <Route path='/admin' element={<Login />} />
           <Route path='/dashboard' element={<Layout />} >
             <Route index element={<Dashboard />} />
-            <Route path='/dashboard/pending' element={<Pending />} />
-            <Route path='/dashboard/package' element={<Packages />} />
-            <Route path='/dashboard/courier' element={<Couriers />} />
-            <Route path='/dashboard/settings' element={<Settings />} />
-          </Route>
+            
+        </Route>
         </Routes>
       </Router>
+    </div>
   )
 }
 
--- src/routes/private/index.jsx
+++ src/routes/private/index.jsx
@@ -10,13 +10,13 @@ import Settings from '../../dashboards/supportFeatures/settings/components';
 function Private() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />} >
-        <Route index element={<Dashboard />} />
-        <Route path='/pending' element={<Pending />} />
-        <Route path='/package' element={<Packages />} />
-        <Route path='/courier' element={<Couries />} />
-        <Route path='/settings' element={<Settings />} />
-      </Route>
+        <Route path='/' element={<Layout />} >
+            <Route index element={<Dashboard />} />
+            <Route path='pending' element={<Pending />} />
+            <Route path='package' element={<Packages />} />
+            <Route path='courier' element={<Couries />} />
+            <Route path='settings' element={<Settings />} />
+        </Route>
     </Routes>
   )
 }
